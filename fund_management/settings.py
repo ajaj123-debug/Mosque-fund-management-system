3a6155499e872a25b50f5311f3f417eb23a9b782
ajaj123-debug <ajaj42699@gmail.com>
2024-10-05 03:03:01 +0530

    first commit

--- fund_management/settings.py
+++ fund_management/settings.py
@@ -11,9 +11,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-_mwi%3p2j)=&*n^@aj@w$!@$$#8d#_p5^qmh6$)_o)g&8i)-z@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition

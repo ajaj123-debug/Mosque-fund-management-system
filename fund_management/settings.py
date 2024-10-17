fda2cd555286451132f99f41242bc4e407d7ec82
ajaj123-debug <ajaj42699@gmail.com>
2024-10-18 03:09:19 +0530

    last commit

--- fund_management/settings.py
+++ fund_management/settings.py
@@ -17,7 +17,11 @@ ALLOWED_HOSTS = ['*']
 
 import os
 STATIC_URL = '/static/'
-if not DEBUG:    # Tell Django to copy static assets into a path called `staticfiles` (this is specific to Render)
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+if not DEBUG:
+    # Tell Django to copy static assets into a path called `staticfiles` (this is specific to Render)
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
     # Enable the WhiteNoise storage backend, which compresses static files to reduce disk use
     # and renames the files with unique names for each version to support long-term caching

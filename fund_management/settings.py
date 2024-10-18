94feda5aa6bdeb2fd0f7aa00cf39b0dfd0b83c3e
ajaj123-debug <ajaj42699@gmail.com>
2024-10-18 14:08:21 +0530

    last commit

--- fund_management/settings.py
+++ fund_management/settings.py
@@ -82,18 +82,18 @@ WSGI_APPLICATION = 'fund_management.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
-from decouple import config
-import dj_database_url
 DATABASES = {
-    "default": dj_database_url.config(default=config("DATABASE_URL"))
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
+
+# from decouple import config
+# import dj_database_url
+# DATABASES = {
+#     "default": dj_database_url.config(default=config("DATABASE_URL"))
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators

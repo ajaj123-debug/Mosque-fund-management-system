8eddaac5d8d3e162e3f8b71ae869512a55f38356
ajaj123-debug <ajaj42699@gmail.com>
2024-10-19 23:41:26 +0530

    last commit

--- community_fund/admin.py
+++ community_fund/admin.py
@@ -3,3 +3,6 @@ from .models import Deduction, User, Transaction
 admin.site.register(User)
 admin.site.register(Transaction)
 admin.site.register(Deduction)
+admin.site.site_title = "Sunni Noori Jama Masjid"
+admin.site.index_title = "Sunni Noori Jama Masjid"
+admin.site.site_header = "Masjid Fund Itwa, Saran"

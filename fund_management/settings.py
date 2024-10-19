f290769af484c5e639396c852570575330d8eca0
ajaj123-debug <ajaj42699@gmail.com>
2024-10-19 21:42:39 +0530

    last commit

--- fund_management/settings.py
+++ fund_management/settings.py
@@ -11,15 +11,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-_mwi%3p2j)=&*n^@aj@w$!@$$#8d#_p5^qmh6$)_o)g&8i)-z@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
-import os
-STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
+# import os
+# STATIC_URL = '/static/'
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'static'),
+# ]
 
 # Application definition
 
@@ -129,26 +129,38 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
-import os
+# import os
 
-# Path where static files will be collected during `collectstatic`
-# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # Ensure BASE_DIR is defined in your settings
+# # Path where static files will be collected during `collectstatic`
+# # STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # Ensure BASE_DIR is defined in your settings
 
-STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / 'static']
-import os
+# STATIC_URL = '/static/'
+# STATICFILES_DIRS = [BASE_DIR / 'static']
+# import os
 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+# BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'community_fund', 'static'),  # Update this line
-]
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'community_fund', 'static'),  # Update this line
+# ]
 
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# STATIC_URL = '/static/'
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+from pathlib import Path
+import os
+
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+# Static files settings
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'community_fund', 'static'),  # Static files for the community_fund app
+]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # Location for collectstatic

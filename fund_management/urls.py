c6b8865a39de14f821995c8c026c9050c85cb465
ajaj123-debug <ajaj42699@gmail.com>
2024-10-22 00:20:41 +0530

    ETIF

--- fund_management/urls.py
+++ fund_management/urls.py
@@ -7,7 +7,6 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # path('login/', views.login, name='login'),
     path('login/', views.user_login, name='login'),
     path('', views.index, name='index'),
     path('home/', views.home, name='home'),
@@ -18,5 +17,5 @@ urlpatterns = [
     path('generate_report/', views.generate_report, name='generate_report'),
     path('add_transaction/', views.add_transaction, name='add_transaction'),
     path('generate_user_report/', views.generate_user_report, name='generate_user_report'),
-    path('monthwise-report/', monthwise_report, name='monthwise_report')    
+    path('monthwise-report/', monthwise_report, name='monthwise_report'),
 ]
